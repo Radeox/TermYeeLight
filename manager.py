041546ee338524ea92317b85a84d8221fd434db8
Dawid Weglarz <dawid.weglarz95@gmail.com>
2022-04-20 00:15:18 +0200

    Some code clean up

--- manager.py
+++ manager.py
@@ -1,12 +1,12 @@
 import sys
-import time
-from yeelight import Bulb
-from yeelight.main import BulbException
+
+import yeelight
+
 from settings import IP
 
 
 class SmartBulb:
-    mode = {
+    MODE = {
         "default": {
             "brightness": 66,
             "color_temp": 3000,
@@ -16,7 +16,7 @@ class SmartBulb:
             "color_temp": 2000,
         },
         "red": {
-            "brightness": 33,
+            "brightness": 10,
             "RGB": (255, 125, 125),
         },
         "work": {
@@ -24,68 +24,59 @@ class SmartBulb:
             "color_temp": 7000,
         },
         "gaming": {
-            "brightness": 20,
+            "brightness": 10,
             "RGB": (0, 0, 255),
         },
     }
 
     def __init__(self, ip_address):
-        self.bulb = Bulb(ip_address)
-        self.status = self.bulb.get_properties()['power']
+        self.bulb = yeelight.Bulb(ip_address)
+        self.on = self.bulb.get_properties()['power'] == 'on'
 
     def set_power(self, power):
         if power:
-            self.status = 'on'
+            self.on = True
             self.bulb.turn_on()
-            print("Turning on.")
         else:
-            self.status = 'off'
+            self.on = False
             self.bulb.turn_off()
-            print("Turning off.")
 
     def toggle_power(self):
-        if self.is_on:
-            self.set_power(False)
-        else:
-            self.set_power(True)
+        self.set_power(not self.on)
 
     def set_mode(self, mode):
-        if mode not in self.mode:
-            print("Wrong mode!\nAvailable modes:")
-            for mode in self.mode.keys(): print(f"- {mode}")
-            return
-        elif self.is_off:
+        # If bulb is off, turn it on
+        if not self.on:
             self.set_power(True)
 
-        brightness = self.mode[mode]['brightness']
-        color_temp = self.mode[mode].get('color_temp')
-        rgb = self.mode[mode].get('RGB')
-
+        # Set bulb properties
+        brightness = self.MODE[mode]['brightness']
+        color_temp = self.MODE[mode].get('color_temp')
         self.bulb.set_brightness(brightness)
+        rgb = self.MODE[mode].get('RGB')
+
         if rgb:
             self.bulb.set_rgb(*rgb)
         else:
             self.bulb.set_color_temp(color_temp)
 
-        print(f"Setting mode '{mode}'.")
-
-    @property
-    def is_on(self):
-        return True if self.status == 'on' else False
-
-    @property
-    def is_off(self):
-        return True if self.status == 'off' else False
-
 
 if __name__ == "__main__":
     try:
         bulb = SmartBulb(IP)
-    except BulbException as e:
+    except yeelight.main.BulbException as e:
         print(f"Yee error: {e}")
     else:
         if len(sys.argv) > 1:
-            param = sys.argv[1]
-            bulb.set_mode(param)
+            mode = sys.argv[1]
+
+            if mode not in bulb.MODE:
+                print("Wrong mode!\nAvailable modes:")
+                for mode in bulb.MODE.keys():
+                    print(f"- {mode}")
+            else:
+                print(f"Setting mode '{mode}'.")
+                bulb.set_mode(mode)
         else:
+            print("Toggling power.")
             bulb.toggle_power()
